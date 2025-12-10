a80ced34a8b82f110a935f6aae31dfc87f7de1f0
Bananacookie11 <pornsin.sr@kkumail.com>
2025-12-10 21:49:49 +0700

    lab03 commit

---
+++ docs/lab03_typescript/src/managers/StudentManager.ts
@@ -0,0 +1,60 @@
+import { Student } from "../models/Student.js";
+
+
+export class StudentManager {
+  private students: Student[] = [];
+
+
+  addStudent(student: Student): void {
+    this.students.push(student);
+    this.saveToLocalStorage();
+  }
+
+
+  getAllStudents(): Student[] {
+    return this.students;
+  }
+
+
+  findStudentByID(id: string): Student | undefined {
+    return this.students.find(s => s.id === id);
+  }
+
+
+  findStudentsByName(name: string): Student[] {
+    const lowercasedName = name.toLowerCase(); 
+    return this.students.filter(s =>
+      s.first_name.toLowerCase().includes(lowercasedName) ||
+      s.last_name.toLowerCase().includes(lowercasedName)
+    );
+  }
+
+
+  findStudentsByMajor(major: string): Student[] {
+    return this.students.filter(s =>
+      s.major.toLowerCase().includes(major.toLowerCase())
+    );
+  }
+
+
+  saveToLocalStorage(): void {
+    localStorage.setItem("students", JSON.stringify(this.students));
+  }
+
+
+  loadFromLocalStorage(): void {
+    const data = localStorage.getItem("students");
+    if (data) {
+      this.students = JSON.parse(data);
+    }
+  }
+
+   findStudentByEmail(email: string): Student | undefined {
+    const lowercasedEmail = email.toLowerCase();
+    return this.students.find(s => 
+      // นำเงื่อนไขการเปรียบเทียบอีเมลมาใส่ตรงนี้
+      s.email.toLowerCase() === lowercasedEmail
+    );
+  }
+
+}
---
+++ docs/lab03_typescript/src/models/Student.ts
@@ -0,0 +1,9 @@
+export interface Student {
+  id: string;
+  title_name: string; // สำหรับเก็บคำนำหน้าชื่อ
+  first_name: string; // สำหรับเก็บชื่อจริง
+  last_name: string;  // สำหรับเก็บนามสกุล
+  year: number;
+  major: string;
+  email: string;      
+}
